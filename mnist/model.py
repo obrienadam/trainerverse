ae74a4d75d0f2f266ddf86543cd6011476820aa6
Adam O'Brien <obrienadam89@gmail.com>
2025-08-13 04:20:00 -0700

    Initial commit. Have basic working CNN model.

---
+++ mnist/model.py
@@ -0,0 +1,44 @@
+import flax.linen as nn
+import flax
+import jax
+import jax.numpy as jnp
+from absl import logging as log
+
+@flax.struct.dataclass
+class ModelConfig:
+    kernel_size = (5, 5)
+    filters = (32, 32, 64, 64)
+    mlp_dims = (64,)
+    window_size = (2, 2)
+
+class Model(nn.Module):
+    config: ModelConfig = ModelConfig()
+
+    @nn.compact
+    def __call__(self, x: jax.Array):
+        
+        for filter_size in self.config.filters:
+            x = nn.Conv(
+                features = filter_size,
+                kernel_size = self.config.kernel_size
+            )(x)
+            x = nn.max_pool(x, self.config.window_size)
+
+        x = jnp.reshape(x, (x.shape[0], -1))
+        for dims in self.config.mlp_dims:
+            x = nn.Dense(dims)(x)
+            x = nn.relu(x)
+
+        x = nn.Dense(10)(x)
+        return x
+
+if __name__ == "__main__":
+    m = Model(ModelConfig())
+    input = jnp.ones((1, 32, 32, 1))
+
+    key = jax.random.key(42)
+    param_key, other_key = jax.random.split(key, 2)
+
+    variables = m.init(param_key, input)
+    
+    print(m.apply(variables, input))
---
+++ mnist/train.py
@@ -0,0 +1,55 @@
+import model
+from data import load_dataset, batch_generator
+import jax
+import optax
+import jax.numpy as jnp
+import jax_metrics
+
+def loss_fn(func, variables, x, labels):
+    logits = func(variables, x)
+    return jnp.mean(optax.losses.softmax_cross_entropy_with_integer_labels(logits, labels)), logits
+
+def train_step(func, variables, x, labels, update_fn, opt_state):
+    (loss, _), grads = jax.value_and_grad(loss_fn, argnums=1, has_aux=True)(func, variables, x, labels)
+
+    updates, opt_state = update_fn(grads, opt_state)
+    variables = optax.apply_updates(variables, updates)
+
+    return loss, variables, opt_state
+
+train_step = jax.jit(train_step, static_argnames=('func', 'update_fn'))
+
+def eval_step(func, variables, x, labels):
+    return loss_fn(func, variables, x, labels)
+
+eval_step = jax.jit(eval_step, static_argnames=('func',))
+
+def train():
+    df_train, df_test = load_dataset()
+    data_itr = batch_generator(df_train, 64)
+
+    m = model.Model()
+    key = jax.random.key(2342)
+
+    optimizer = optax.adam(learning_rate=1e-3)
+    variables = m.init(key, jnp.empty((1, 28, 28, 1)))
+    opt_state = optimizer.init(variables)
+
+    for batch, labels in data_itr:
+        loss, variables, opt_state = train_step(m.apply, variables, batch, labels, optimizer.update, opt_state)
+        print(loss)
+
+    metrics = jax_metrics.metrics.Accuracy(num_classes=10)
+
+    for batch, labels in batch_generator(df_test, 64):
+        loss, logits = eval_step(m.apply, variables, batch, logits)
+        metrics.update(logits, labels)
+
+    print(metrics.compute())
+
+
+if __name__ == "__main__":
+    train()
+
+
+

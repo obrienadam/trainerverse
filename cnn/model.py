6e8bdd4101c379f4257b909e070c7c218c6673ef
Adam O'Brien <obrienadam89@gmail.com>
2025-08-30 01:46:39 -0700

    Added batch norm and fixed some bugs.

--- cnn/model.py
+++ cnn/model.py
@@ -10,17 +10,23 @@ class ModelParams:
     num_outputs: int = 10
     features: Sequence[int] = (32, 64, 128)
     kernel_size: tuple[int, int] = (3, 3)
-    mlp_dims: Sequence[int] = (256, 128)
-    window_shape: tuple[int, int] = (3, 3)
+    mlp_dims: Sequence[int] = (512, 256)
+    window_shape: tuple[int, int] = (2, 2)
 
 
 class Model(nn.Module):
     params: ModelParams
 
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x, training=True):
         for feature in self.params.features:
-            x = nn.Conv(features=feature, kernel_size=self.params.kernel_size)(x)
+            x = nn.Conv(
+                features=feature,
+                kernel_size=self.params.kernel_size,
+                kernel_init=nn.initializers.kaiming_normal(),
+            )(x)
+            x = nn.BatchNorm(use_running_average=not training)(x)
+            x = nn.relu(x)
             x = nn.max_pool(x, window_shape=self.params.window_shape)
 
         x = x.reshape((x.shape[0], -1))
--- cnn/train.py
+++ cnn/train.py
@@ -2,7 +2,7 @@ from model import ModelParams, Model
 import optax
 import jax
 from flax.struct import dataclass
-from flax.training.train_state import TrainState
+from flax.training import train_state
 import chex
 from data import load_data
 import jax.numpy as jnp
@@ -18,23 +18,39 @@ class HyperParams:
     seed: int = 9234578
 
 
-def loss_fn(params, x, y, apply_fn):
-    logits = apply_fn(params, x)
-    return optax.softmax_cross_entropy_with_integer_labels(logits, y).mean(), logits
+class TrainState(train_state.TrainState):
+    batch_stats: chex.ArrayTree
+
+
+def loss_fn(params, batch_stats, x, y, apply_fn):
+    logits, new_variables = apply_fn(
+        {"params": params, "batch_stats": batch_stats},
+        x,
+        training=True,
+        mutable=["batch_stats"],
+    )
+    return (
+        optax.softmax_cross_entropy_with_integer_labels(logits, y).mean(),
+        (logits, new_variables),
+    )
 
 
 @jax.jit
 def train_step(train_state, x, y):
-    (loss, _), grads = jax.value_and_grad(loss_fn, allow_int=True, has_aux=True)(
-        train_state.params, x, y, train_state.apply_fn
+    (loss, (_, new_variables)), grads = jax.value_and_grad(
+        loss_fn, allow_int=True, has_aux=True
+    )(train_state.params, train_state.batch_stats, x, y, train_state.apply_fn)
+    train_state = train_state.apply_gradients(
+        grads=grads, batch_stats=new_variables["batch_stats"]
     )
-    train_state = train_state.apply_gradients(grads=grads)
     return train_state, loss
 
 
 @jax.jit
 def eval_step(train_state, x, y):
-    loss, logits = train_state.apply_fn(train_state.params, x, y, train_state.apply_fn)
+    loss, (logits, _) = loss_fn(
+        train_state.params, train_state.batch_stats, x, y, train_state.apply_fn
+    )
     return loss, logits
 
 
@@ -47,24 +63,27 @@ def train(
     variables = model.init(rng, jnp.ones([1, 32, 32, 3]))
     tx = optax.adam(learning_rate=hyperparams.learning_rate)
 
-    train_state = TrainState.create(apply_fn=model.apply, params=variables, tx=tx)
+    train_state = TrainState.create(
+        apply_fn=model.apply,
+        params=variables["params"],
+        batch_stats=variables["batch_stats"],
+        tx=tx,
+    )
 
     train_ds, test_ds, ds_info = load_data("cifar10")
+    train_ds = train_ds.shuffle(1_000_000).batch(hyperparams.batch_size)
+    test_ds = test_ds.batch(hyperparams.batch_size)
 
     for epoch in range(hyperparams.num_epochs):
         logging.info("Starting epoch %d / %d.", epoch + 1, hyperparams.num_epochs)
-
-        train_ds = train_ds.shuffle(1024).batch(hyperparams.batch_size)
 
         for step, batch in enumerate(train_ds.as_numpy_iterator()):
             images, labels = batch
             train_state, loss = train_step(train_state, images, labels)
             logging.info("Epoch %d, step %d, loss: %.4f", epoch + 1, step + 1, loss)
 
-        train_ds = train_ds.unbatch()
-
         accuracy = jax_metrics.metrics.Accuracy()
-        for batch in test_ds.batch(hyperparams.batch_size).as_numpy_iterator():
+        for batch in test_ds.as_numpy_iterator():
             images, labels = batch
             loss, logits = eval_step(train_state, images, labels)
             accuracy = accuracy.update(preds=logits, target=labels)
